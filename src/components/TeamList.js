34f2284deffe681c723016e6f981dca3d51c735f
Brock Duncan <bduncan@tkg.com>
2018-05-22 10:10:01 -0400

    add new nomination to state, clear nomination text

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -4,31 +4,72 @@ import Teammate from './Teammate';
 export default class TeamList extends Component {
   constructor(props) {
     super(props);
+    this.onNomination = this.onNomination.bind(this);
     this.state = {
       teammates: [
         {
           id: 1,
-          name: 'Josh Moyer',
-          nominations: 3,
+          name: 'Josh Moyers',
+          nominations: [
+            {
+              nomination: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              author: "Brock Duncan"
+            },
+            {
+              nomination: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              author: "Brock Duncan"
+            }
+          ]
         },
         {
           id: 2,
           name: 'Tim Ruby',
-          nominations: 4,
+          nominations: [
+            {
+              nomination: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              author: "Brock Duncan"
+            }
+          ]
         },
         {
           id: 3,
           name: 'James Golden',
-          nominations: 5,
+          nominations: [
+            {
+              nomination: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              author: "Brock Duncan"
+            },
+            {
+              nomination: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              author: "Brock Duncan"
+            },
+            {
+              nomination: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              author: "Brock Duncan"
+            }
+          ]
         }
       ]
     }
+  }
+  onNomination(id, nomination) {
+    // find teammate
+    let obj = this.state.teammates.findIndex(obj => {
+      return obj.id === id;
+    });
+    // add nomination to teammate in state
+    let teammatesCopy = this.state.teammates;
+    teammatesCopy[obj].nominations.push(nomination);
+    this.setState((prevState) => ({
+      teammates: teammatesCopy
+    }));
+    console.log(this.state.teammates[obj]);
   }
   render() {
     return(
       <ul className="team-list">
         {this.state.teammates.map((teammate) =>
-          <Teammate name={teammate.name} nominations={teammate.nominations} key={teammate.id}/>
+          <Teammate id={teammate.id} name={teammate.name} nominations={teammate.nominations} key={teammate.id} onNomination={this.onNomination}/>
         )}
       </ul>
     )
--- src/components/Teammate.js
+++ src/components/Teammate.js
@@ -5,27 +5,52 @@ export default class Teammate extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isActive: false
+      isActive: false,
+      nominationText: "",
+      nominations: this.props.nominations
     }
     this.toggleActive = this.toggleActive.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleNomination = this.handleNomination.bind(this);
+    this.nominationArea = React.createRef();
   }
   toggleActive() {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
+  handleChange(e) {
+    this.setState({
+      nominationText: e.target.value
+    })
+  }
+  handleNomination() {
+    if(this.state.nominationText.length > 0) {
+      this.props.onNomination(this.props.id, {nomination: this.state.nominationText, author: "Brock"});
+      this.toggleActive();
       this.setState({
-        isActive: !this.state.isActive
-      })
+        nominationText: ""
+      });
+      this.nominationArea.current.value = "";
+    } else {
+      alert('enter a nomination');
+    }
   }
   render() {
     return(
       <li className={"teammate " + (this.state.isActive ? "teammate--active" : "")}>
         <div className="teammate__info">
           <span className="teammate__name">{this.props.name}</span>
-          <span className="teammate__nominations">{this.props.nominations}</span>
+          <span className="teammate__nominations">{this.props.nominations.length}</span>
           <span className="teammate__nominate" onClick={this.toggleActive}><i className="fas fa-trophy"></i> Nominate</span>
         </div>
         <div className="teammate__nominate-panel">
           <h2>Nominate {this.props.name}</h2>
-          <textarea className="teammate__nominate-textarea" placeholder="Why are you nominating them?"></textarea>
-          <span className="teammate__nominate-submit" onClick={this.submitNomination}><i className="fas fa-paper-plane"></i> Submit Nomination</span>
+          <textarea className="teammate__nominate-textarea"
+                    placeholder="Why are you nominating them?"
+                    onChange={this.handleChange}
+                    ref={this.nominationArea}></textarea>
+                  <span className="teammate__nominate-submit" onClick={this.handleNomination}><i className="fas fa-paper-plane"></i> Submit Nomination</span>
           <span className="teammate__nominate-close" onClick={this.toggleActive}><i className="fas fa-times"></i></span>
         </div>
       </li>
@@ -35,5 +60,6 @@ export default class Teammate extends Component {
 
 Teammate.propTypes = {
   name: PropTypes.string.isRequired,
-  nominations: PropTypes.number.isRequired
+  nominations: PropTypes.array.isRequired,
+  onNomination: PropTypes.func.isRequired
 }
